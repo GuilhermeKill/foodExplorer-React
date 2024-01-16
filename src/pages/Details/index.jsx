17af32004c6100ba867f115c01b078faa4b031f7
Guilherme Reis Kill <guilherme.reiskill2002@gmail.com>
2024-01-16 01:16:50 -0300

    Update index.jsx

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -34,7 +34,7 @@ export function Details() {
       <ButtonBack />
       <Content>
         {data.image &&
-          <DescriptionSection src={`https://food-explorer-api-zmmj.onrender.com/files/dishfiles/${data.image}`} data={data} />
+          <DescriptionSection src={`https://food-explorer-api-6wpc.onrender.com/files/dishfiles/${data.image}`} data={data} />
         }
       </Content>
       <Footer />
