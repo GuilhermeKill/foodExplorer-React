e33d2dd8e72ff08c1a65d5c3d2bf8fdd96579754
Guilherme Reis Kill <guilherme.reiskill2002@gmail.com>
2024-01-16 01:14:38 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: "https://food-explorer-api-zmmj.onrender.com"
+    baseURL: "https://food-explorer-api-6wpc.onrender.com"
 })
